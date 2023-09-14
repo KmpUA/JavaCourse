345d2b0faf2e0b37d281ea19f77a398b39040e3a
KmpUA <delamakcima1@gmail.com>
2023-09-14 18:50:11 +0300

    Lab0 final variant

--- src/main/java/org/lab0/Variant17.java
+++ src/main/java/org/lab0/Variant17.java
@@ -1,22 +1,12 @@
 package org.lab0;
 
-import java.lang.reflect.Array;
-import java.util.Locale;
+import java.util.*;
 
 public class Variant17 {
 	/**
 	 *
-	 * @param k is square side
-	 * @return perimeter
-	 */
-	public int inputOutputTask(int k) {
-		return 0;
-	}
-
-	/**
-	 *
-	 * @param k is distance in cm
-	 * @return distance in m
+	 * @param k is an integer
+	 * @return hundreds of number
 	 */
 
 	public int integerNumbersTask(int k) {
@@ -26,8 +16,8 @@ public class Variant17 {
 
 	/**
 	 *
-	 * @param number1
-	 * @return true, if number is possitive
+	 * @param number1 given number
+	 * @return true, if number is odd and it is bigger than 99
 	 */
 	public boolean booleanTask(int number1) {
 		if(number1 < 0) {
@@ -38,9 +28,9 @@ public class Variant17 {
 
 
 	/**
-	 *
-	 * @param a, b, c are integer number
-	 * @return transformed number
+	 *If a, b, c is a progression return them * on 2, else with - sign
+	 * @param a, b, c are double numbers
+	 * @return array of result
 	 */
 	public double[] ifTask(double a, double b, double c) {
 		return (a > b && b > c || a < b && b < c) ? new double[]{2 * a, 2 * b, 2 * c}: new double[]{-a, -b, -c};
@@ -48,15 +38,13 @@ public class Variant17 {
 
 
 	/**
-	 *
-	 * @param number1
-	 * @return day of week day represented number1
+	 *We have to return a correct pronunciation of nums from 10 to 40
+	 * @param number1 is a given number
+	 * @return text form of given number
 	 */
 	public String switchTask(int number1) {
 		String result = "";
-		if(number1 < 10 || number1 > 40){
-			throw  new IllegalArgumentException("Number is less than 10 or bigger than 40");
-		}
+		if(number1 < 10 || number1 > 40) throw new IllegalArgumentException("Number is less than 10 or bigger than 40");
 		if(number1 / 10 == 1){
 			switch (number1) {
 				case 10 -> result += "Десять ";
@@ -77,7 +65,7 @@ public class Variant17 {
 				case 3 -> result += "Тридцять ";
 				case 4 -> result += "Сорок ";
 			}
-			switch (number1 % 10){
+			switch (number1 % 10) {
 				case 1 -> result += "одне ";
 				case 2 -> result += "два ";
 				case 3 -> result += "три ";
@@ -88,51 +76,88 @@ public class Variant17 {
 				case 8 -> result += "вісім ";
 				case 9 -> result += "дев'ять ";
 			}
-			switch (number1 % 10) {
-				case 1 -> result += "учбове завдання";
-				case 2, 3, 4 -> result += "учбових завдання";
-				case 5, 6, 7, 8, 9 -> result += "учбових завдань";
-			}
+		}
+		switch (number1 % 10) {
+			case 1 -> result += "учбове завдання";
+			case 2, 3, 4 -> result += "учбових завдання";
+			case 5, 6, 7, 8, 9 -> result += "учбових завдань";
 		}
 		return result;
 	}
 
 
 	/**
-	 *
-	 * @param n is integer number
-	 * @return approximated value of exp(1)
+	 *We have to sum num A in pow of 0 to n
+	 * @param A is integer number
+	 * @param n is a given power
+	 * @return final sum
 	 */
-	public double forTask(int n) {
-		assert n >0: "Argument should be more than zero";
-		return 0;
-	}
-
-
-	public int whileTask(int a, int b) {
-		assert (a >0 && b > 0): "Argument should be more than zero";
-		return 0;
-	}
-
-	public double arrayTask(double[] array) {
-		return 0;
+	public double forTask(int A, int n) {
+		if(n <= 0) throw new IllegalArgumentException("n <= 0");
+		int result = 0;
+		for(int i = 0; i < n; i++){
+			result += (int) Math.pow(A, i);
+		}
+		return result;
 	}
 
 	/**
-	 *
-	 * @param array
-	 * @param k1
-	 * @param k2
-	 * @return transformed array where row with indexes k1 and k2 was changed
+	 * Task is to return list of digits of given number in reverse
+	 * @param N given number
+	 * @return List of reversed digits of N
 	 */
-	public int[][]  twoDimensionArrayTask(int[][] array, int k1, int k2) {
-		//return null;
-		return array;
+	public List<Integer> whileTask(int N) {
+		if(N <= 0) throw new IllegalArgumentException("N <= 0");
+		List<Integer> result = new ArrayList<>();
+		while(N > 0){
+			result.add(N % 10);
+			N /= 10;
+		}
+		return result;
+	}
+
+	/**
+	 * @exception IllegalArgumentException if size of array <= 0
+	 * @param A array of N size with type of double
+	 * @param N size of array
+	 * @return List of elements of given array with this pattern: A1, A2, AN, AN-1, A3 ...
+	 */
+
+	public List<Double> arrayTask(double[] A, int N) {
+		if(N <= 0) throw new IllegalArgumentException("N <= 0");
+		List<Double> result = new ArrayList<>();
+		int L = 0;
+		int R = N - 1;
+		while(L <= R){
+			result.addAll(new ArrayList<>(Arrays.asList(A[L], A[L + 1], A[R], A[R - 1])));
+			L += 2;
+			R -= 2;
+		}
+		return result;
+	}
+
+	/**
+	 * @exception IllegalArgumentException if K is not 0 < K < M
+	 * @param array given matrix
+	 * @param M count of rows
+	 * @param N count of columns
+	 * @param K given parameter
+	 * @return sum and product of K-row elements
+	 */
+	public List<Integer>  twoDimensionArrayTask(int[][] array, int M, int N, int K) {
+		if(K >= M || K <= 0 || N <= 0) throw new IllegalArgumentException("K <= 0 OR K >= M");
+		int sum = 0;
+		int product = 1;
+		for (var elem:array[K - 1]) {
+			sum += elem;
+			product *= elem;
+		}
+		return new ArrayList<>(Arrays.asList(sum, product));
 	}
 
 	public static void main(String... strings) {
 		System.out.println("Start of zero lab");
 		System.out.println("Done!!!");
-		System.out.println(new Variant17().switchTask(15));
+		System.out.println(new Variant17().twoDimensionArrayTask(new int[][]{{1, 2, 3, 4}, {-4, -5, -6, -0}, {-12, 32, 41, 0}, {9, 8, 7, 6}}, 4, 4, 3));
 	}
 }
--- src/test/java/org/lab0/Variant17Test.java
+++ src/test/java/org/lab0/Variant17Test.java
@@ -3,8 +3,10 @@ package org.lab0;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.util.Currency;
-import java.util.Locale;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
@@ -45,13 +47,39 @@ public class Variant17Test {
 		};
 	}
 
-	@Test
-	public void testInputOutputTask() {
+	@DataProvider
+	public static Object[][] forProvider() {
+		return new Object[][]{
+				{10, 4, 1111},
+				{5, 12, 61035156},
+				{2, 5, 31}
+		};
 	}
 
-	@Test(dataProvider = "integerProvider")
-	public void testIntegerNumbersTask(int k, int l) {
-		assertEquals(new Variant17().integerNumbersTask(k), l);
+	@DataProvider
+	public static Object[][] whileProvider() {
+		return new Object[][]{
+				{1234, new ArrayList<>(Arrays.asList(4, 3, 2, 1))},
+				{901, new ArrayList<>(Arrays.asList(1, 0, 9))},
+				{92781034, new ArrayList<>(Arrays.asList(4, 3, 0, 1, 8, 7, 2, 9))}
+		};
+	}
+
+	@DataProvider
+	public static Object[][] arrayProvider() {
+		return new Object[][]{
+				{new double[]{1, 2, 3, 4, 5}, 5, new ArrayList<>(Arrays.asList(1d, 2d, 5d, 4d, 3d, 4d, 3d, 2d))},
+				{new double[]{9, 0, -1, 12}, 4, new ArrayList<>(Arrays.asList(9d, 0d, 12d, -1d))},
+				{new double[]{1234, -567, 12}, 3, new ArrayList<>(Arrays.asList(1234d, -567d, 12d, -567d))}
+		};
+	}
+
+	@DataProvider
+	public static Object[][] matrixProvider() {
+		return new Object[][]{
+				{new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 3, 3, 2, new ArrayList<>(Arrays.asList(15, 120))},
+				{new int[][]{{1, 2, 3, 4}, {-4, -5, -6, -0}, {-12, 32, 41, 0}, {9, 8, 7, 6}}, 4, 4, 3, new ArrayList<>(Arrays.asList(61, 0))}
+		};
 	}
 
 	@Test(expectedExceptions = AssertionError.class)
@@ -59,15 +87,47 @@ public class Variant17Test {
 		new Variant17().integerNumbersTask(999);
 	}
 
-	@Test(dataProvider = "booleanProvider")
-	public void testBooleanTask(int number1, boolean result) {
-		assertEquals(new Variant17().booleanTask(number1), result);
+	@Test(dataProvider = "integerProvider")
+	public void testIntegerNumbersTask(int k, int l) {
+		assertEquals(new Variant17().integerNumbersTask(k), l);
 	}
 
 	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testIllegalBooleanTask(){
 		new Variant17().booleanTask(-1);
 	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void illegalNumSwitchTest(){
+		new Variant17().switchTask(9);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testIllegalForTask(){
+		new Variant17().forTask(5, 0);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testIllegalWhileTask(){
+		new Variant17().whileTask(0);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testIllegalArrayTask(){
+		new Variant17().arrayTask(new double[]{1, 2, 3, 4, 5}, -1);
+	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void testIllegalMatrixTask(){
+		new Variant17().twoDimensionArrayTask(new int[][]{}, 0, 5, -1);
+	}
+
+	@Test(dataProvider = "booleanProvider")
+	public void testBooleanTask(int number1, boolean result) {
+		assertEquals(new Variant17().booleanTask(number1), result);
+	}
+
+
 
 	@Test(dataProvider = "ifProvider")
 	public void testIfTask(double a, double b, double c, double[] result) {
@@ -80,24 +140,31 @@ public class Variant17Test {
 		assertEquals(new Variant17().switchTask(number1), result);
 	}
 
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void illegalNumSwitchTest(){
-		new Variant17().switchTask(9);
+
+
+	@Test(dataProvider = "forProvider")
+	public void testForTask(int A, int n, int result) {
+		assertEquals(new Variant17().forTask(A, n), result);
 	}
 
-	@Test
-	public void testForTask() {
+
+
+	@Test(dataProvider = "whileProvider")
+	public void testWhileTask(int N, List<Integer> result) {
+		assertEquals(new Variant17().whileTask(N), result);
 	}
 
-	@Test
-	public void testWhileTask() {
+
+
+	@Test(dataProvider = "arrayProvider")
+	public void testArrayTask(double[] A, int N, List<Double> result) {
+		assertEquals(new Variant17().arrayTask(A, N), result);
 	}
 
-	@Test
-	public void testArrayTask() {
-	}
 
-	@Test
-	public void testTwoDimensionArrayTask() {
+
+	@Test(dataProvider = "matrixProvider")
+	public void testTwoDimensionArrayTask(int[][] array, int M, int N, int K, List<Integer> result) {
+		assertEquals(new Variant17().twoDimensionArrayTask(array, M, N, K), result);
 	}
 }
